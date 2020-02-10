468b1921b1e14da00c8793d63f4ab00e4cb80375
lihaiyuan <380420978@qq.com>
2020-02-10 23:24:16 +0800

    es6

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*
+ * @Description: 
+ * @Autor: lihaiyuan
+ * @Email: lihaiyuan@goldenfintech.com.cn
+ * @Date: 2020-02-07 20:16:49
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -13,3 +19,8 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+/* 
+创建项目也可以用 Runtime-only 创建
+也可以用Runtime-Compiler 两种方法。
+但是用Runtime-only 效率更高，代码量更小。
+*/
